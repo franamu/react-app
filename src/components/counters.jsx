c2f588c5182f41862199b04425f588595b52d351
AMUCF001 <Francisco.X.Amuchastegui.-ND@Disney.com>
2020-01-30 06:11:00 -0800

    refactorizo counter para dejarle los metodos de incremento al componente padre counters

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,11 +10,48 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({
+      counters
+    });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({
+      counters
+    });
+    console.log(this.state.counters[index]);
+  };
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} selected />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          />
         ))}
       </div>
     );
